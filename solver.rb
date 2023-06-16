c43b2488696872aa92bb056ae51734cf37d6d59b
cosmosha <cosmoshagan@gmail.com>
2023-06-16 22:01:42 +0000

    Add fizzbuzz implementation

--- solver.rb
+++ solver.rb
@@ -11,4 +11,16 @@ class Solver
     string.reverse
   end
 
+  def fizzbuzz(num)
+    if (num % 3).zero? && (num % 5).zero?
+      'FizzBuzz'
+    elsif (num % 3).zero?
+      'Fizz'
+    elsif (num % 5).zero?
+      'Buzz'
+    else
+      num
+    end
+  end
+
 end
