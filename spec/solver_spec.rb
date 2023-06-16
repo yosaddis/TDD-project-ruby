b2b489c867471263e658832ce146571102990b45
yosaddis <email.to.yoseph@gmail.com>
2023-06-17 00:35:58 +0300

    Add solver_spec file

---
+++ spec/solver_spec.rb
@@ -0,0 +1,11 @@
+require './solver'
+
+describe Solver do
+  solver = Solver.new
+
+  it 'solver should be an instance of Solver' do
+    expect(solver).to be_an_instance_of(Solver)
+  end
+
+ 
+end
